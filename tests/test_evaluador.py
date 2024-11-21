913b7b9f04f7b785b4a89b3551ff6b1681c11e1f
fredyuide <frguamango@uide.edu.ec>
2024-11-20 20:15:10 -0500

    Update test_evaluador.py

--- tests/test_evaluador.py
+++ tests/test_evaluador.py
@@ -1,1 +1,27 @@
+import unittest
+from src.evaluador import evaluar_expresion
 
+class TestEvaluador(unittest.TestCase):
+    
+    def test_expresion_simple(self):
+        expresion = "3 + 5"
+        self.assertEqual(evaluar_expresion(expresion), 8)
+
+    def test_expresion_con_parentesis(self):
+        expresion = "3 + (2 - 8)"
+        self.assertEqual(evaluar_expresion(expresion), -3)
+
+    def test_expresion_multiplicacion(self):
+        expresion = "3 * 5"
+        self.assertEqual(evaluar_expresion(expresion), 15)
+
+    def test_expresion_compleja(self):
+        expresion = "3 + 5 * (2 - 8)"
+        self.assertEqual(evaluar_expresion(expresion), -13)
+
+    def test_expresion_division(self):
+        expresion = "10 / 2"
+        self.assertEqual(evaluar_expresion(expresion), 5)
+
+if __name__ == '__main__':
+    unittest.main()
